ee3e196dfebc65c27c52bedd82cbe34605204542
Steve Franchak <stevefranchak@gmail.com>
2018-03-14 21:15:36 -0400

    Initial commit with files generated from 'electron-forge init --template=react' with some minor changes. Restructured project layout within src. Wait to show the BrowserWindow until React's App component is mounted.

---
+++ src/js/ui/components/app.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+const { ipcRenderer } = require('electron');
+
+export default class App extends React.Component {
+  componentDidMount() {
+    ipcRenderer.send('show');
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Welcome to React!</h2>
+      </div>
+    );
+  }
+}
---
+++ src/js/ui/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {AppContainer} from 'react-hot-loader';
+import App from './components/app';
+
+const render = () => {
+  ReactDOM.render(
+    <AppContainer><App /></AppContainer>,
+    document.getElementById('App')
+  );
+}
+
+render();
+if (module.hot) {
+  module.hot.accept(render);
+}
